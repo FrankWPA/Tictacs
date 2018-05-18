58be6d4003272c4944483005cc4120f92b6261d5
Raffaele <raffa_parise@hotmail.com>
2018-05-18 18:06:05 -0300

    Added Stats and classes

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    [Header("Basic Stats")]
+    public new string name = "Error";
+    public int hp = 5;
+    public int movement = 4;
+
+    [Space(10)]
+
+    [Header ("Attributes")]
+    public int Str = 1;
+    public int Int = 1;
+    public int Agi = 1;
+    public int Des = 1;
+}
---
+++ Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : Character {
+
+    [Header("Presets")]
+    public enemy enemyPressetToApply = enemy.noone;
+
+    [ContextMenu("Apply Enemy Preset")]
+    public void applyEnemyPreset()
+    {
+        switch (enemyPressetToApply)
+        {
+            case enemy.zombie:
+                name = "Zombie";
+                hp = 5; movement = 3;
+                Str = 1; Int = 0; Agi = 0; Des = 0;
+                break;
+
+            case enemy.goblin:
+                name = "Goblin";
+                hp = 3; movement = 5;
+                Str = 1; Int = 0; Agi = 1; Des = 0;
+                break;
+
+            case enemy.kobold:
+                name = "Kobold";
+                hp = 3; movement = 5;
+                Str = 0; Int = 1; Agi = 1; Des = 0;
+                break;
+
+            default: Str = 1; Int = 1; Agi = 1; Des = 1; break;
+        }
+    }
+}
+
+
+// List of Enemy Presets
+public enum enemy
+{
+    noone, zombie, goblin, kobold
+}
