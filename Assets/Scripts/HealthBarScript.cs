5bfb1c3f1afaed3fcfc4efb6aa3cee16dc71795d
Zebigbos <raffa_parise@hotmail.com>
2018-06-20 18:53:09 -0300

    Added health bars

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour {
+
+    public Character character;
+    public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slider.maxValue = character.hp;
+        slider.value = character.currentHp;
+
+        transform.LookAt(Camera.main.transform);
+	}
+}
---
+++ Assets/Scripts/SelectedCharArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedCharArrow : MonoBehaviour {
+
+    float angle = 0;
+    float floatSpeed = 0.1f;
+    float floatAmplitude = 0.2f;
+    float rotateSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.eulerAngles += new Vector3(0, rotateSpeed, 0);
+
+        angle = angle + floatSpeed < 360 ? angle + floatSpeed : angle + floatSpeed - 360;
+
+        transform.position = new Vector3(0, Mathf.Sin(angle) * floatAmplitude, 0) + transform.parent.position;
+
+		
+	}
+}
